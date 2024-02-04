18afcc6e591209c6347a978877b929db48a11883
Yago Costa <yagocosta@Laptop-de-Yago.local>
2024-02-04 17:51:40 -0300

    feat: created not found page

---
+++ client/src/api/modules/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import Header from '../LeagueOfLegendGame/components/Header';
+import { Container, Typography, Box, Button } from '@mui/material';
+import { checkCookiesPermission, getLastURLPath } from '../../helper';
+import { useNavigate } from 'react-router-dom';
+
+function NotFoundPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      checkCookiesPermission((res) => {
+          if (res) {
+              navigate('/');
+              return;
+          }
+          getLastURLPath();
+
+          navigate('/blog');
+      });
+  }, [navigate]);
+
+  return (
+    <div>
+      <style>
+        {`  html, body {
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    height: 100%;
+                    background-color: #121212;
+                    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                }`}
+      </style>
+      <Header />
+    </div>
+  );
+}
+
+export default NotFoundPage;
--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,20 @@ import 'react-toastify/dist/ReactToastify.css';
 import Authentication from './api/modules/Authentication/Authentication';
 import Blog from './api/modules/Blog/Blog';
 import Profile from './api/modules/Profile/Profile';
+import NotFoundPage from './api/modules/NotFoundPage/NotFoundPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/desafio_fullstack">
       <Routes>
         <Route path="/" element={<Authentication />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/profile" element={<Profile tabIndex={0}/>} />
         <Route path="/profile/myposts" element={<Profile tabIndex={1}/>} />
         <Route path="/profile/historic" element={<Profile tabIndex={2}/>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
